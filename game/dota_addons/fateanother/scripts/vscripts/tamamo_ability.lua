e25f970dd29bab67d0c9f7aadec2d0a0304d3ba5
hsyoo <hsyoo@sfu.ca>
2015-05-27 23:16:03 -0700

    Bugfix
    - Added 0.1 second casting point to Archer(5th)'s combo in order to
    prevent him from being able to cast combo outside occasionally
    - Updated Tamamo's assets

---
+++ game/dota_addons/fateanother/scripts/vscripts/tamamo_ability.lua
@@ -0,0 +1,38 @@
+function OnArmedUpStart(keys)
+end
+
+function OnFireCharmLoaded(keys)
+end
+
+function OnFreezeCharmLoaded(keys)
+end
+
+function OnGustCharmLoaded(keys)
+end
+
+function OnVoidCharmLoaded(keys)
+end
+
+function OnSoulstreamStart(keys)
+end
+
+function OnSGStart(keys)
+end
+
+function OnMantraStart(keys)
+end
+
+function OnAmaterasuStart(keys)
+end
+
+function OnSpiritTheftAcquired(keys)
+end
+
+function OnSeveredFateAcquired(keys)
+end
+
+function OnPCFAcquired(keys)
+end
+
+function OnWitchcraftAcquired(keys)
+end
