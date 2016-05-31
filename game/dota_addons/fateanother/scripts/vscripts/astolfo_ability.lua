311404fa6cb63985b58bdef647d6f14ee951652c
Steve <hermit1007@gmail.com>
2016-05-30 23:51:35 -0700

    Finish Astolfo Q and W
    Particles still need to be made

--- game/dota_addons/fateanother/scripts/vscripts/astolfo_ability.lua
+++ game/dota_addons/fateanother/scripts/vscripts/astolfo_ability.lua
@@ -51,9 +51,9 @@ function OnVanishDebuffStart(keys)
 		local prop = Attachments:GetCurrentAttachment(target, "attach_sword")
 		prop:RemoveSelf()
 	end
-
-	target:SetModel("models/development/invisiblebox.vmdl")
-	target:SetOriginalModel("models/development/invisiblebox.vmdl")
+	target:AddEffects(EF_NODRAW)
+	--target:SetModel("models/development/invisiblebox.vmdl")
+	--target:SetOriginalModel("models/development/invisiblebox.vmdl")
 	target:EmitSound("Hero_Oracle.PurifyingFlames.Damage")
 end
 
@@ -61,8 +61,10 @@ function OnVanishDebuffEnd(keys)
 	local caster = keys.caster
 	local target = keys.target
 	local ability = keys.ability
-	target:SetModel(target.OriginalModel)
-	target:SetOriginalModel(target.OriginalModel)
+
+	target:RemoveEffects(EF_NODRAW)
+	--target:SetModel(target.OriginalModel)
+	--target:SetOriginalModel(target.OriginalModel)
 
 	if target:GetName() == "npc_dota_hero_queenofpain" then
 		Attachments:AttachProp(target, "attach_sword", "models/astolfo/astolfo_sword.vmdl")
@@ -73,4 +75,67 @@ function OnDownStart(keys)
 	local caster = keys.caster
 	local targetPoint = keys.target_points[1]
 	local ability = keys.ability
+	local damage = keys.Damage
+	local range = keys.Range
+	local attackCount = keys.AttackCount
+	local lockDuration = keys.LockDuration
+	local counter = 1
+
+	giveUnitDataDrivenModifier(caster, caster, "pause_sealenabled", 0.5)
+
+	Timers:CreateTimer(function()
+		if counter > 4 then return end
+		local forwardVec = RotatePosition(Vector(0,0,0), QAngle(0,RandomFloat(12, -12),0), caster:GetForwardVector())
+		local spearProjectile = 
+		{
+			Ability = ability,
+	        EffectName = "particles/custom/false_assassin/fa_quickdraw.vpcf",
+	        iMoveSpeed = range * 5,
+	        vSpawnOrigin = caster:GetOrigin(),
+	        fDistance = range - 100,
+	        fStartRadius = 200,
+	        fEndRadius = 200,
+	        Source = caster,
+	        bHasFrontalCone = true,
+	        bReplaceExisting = true,
+	        iUnitTargetTeam = DOTA_UNIT_TARGET_TEAM_ENEMY,
+	        iUnitTargetFlags = DOTA_UNIT_TARGET_FLAG_NONE,
+	        iUnitTargetType = DOTA_UNIT_TARGET_ALL,
+	        fExpireTime = GameRules:GetGameTime() + 2.0,
+			bDeleteOnHit = false,
+			vVelocity = forwardVec * range * 5
+		}
+		local projectile = ProjectileManager:CreateLinearProjectile(spearProjectile)
+		StartAnimation(caster, {duration=0.2, activity=ACT_DOTA_ATTACK, rate=4.0})
+		caster:EmitSound("Hero_PhantomLancer.Attack")
+		counter = counter + 1
+		return 0.12
+	end)
+
+end
+
+function OnDownHit(keys)
+	local caster = keys.caster
+	local target = keys.target
+	local damage = keys.Damage
+	local ability = keys.ability
+
+	DoDamage(caster, target, damage , DAMAGE_TYPE_MAGICAL, 0, ability, false)
+	ability:ApplyDataDrivenModifier(caster, target, "modifier_down_with_a_touch_slow", {})
+end
+
+function OnDownSlowTier1End(keys)
+	local caster = keys.caster
+	local target = keys.target
+	local ability = keys.ability
+
+	ability:ApplyDataDrivenModifier(caster, target, "modifier_down_with_a_touch_slow_2", {})
+end
+
+function OnDownSlowTier2End(keys)
+	local caster = keys.caster
+	local target = keys.target
+	local ability = keys.ability
+
+	ability:ApplyDataDrivenModifier(caster, target, "modifier_down_with_a_touch_slow_3", {})
 end
